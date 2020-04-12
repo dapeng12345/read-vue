18a4f0a3e8e205fcf6d1789c757cb3b1381da44e
zhangyunpeng <990779323@qq.com>
2020-04-12 11:08:00 +0800

    增加 element

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/standard'
+	],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'vue/no-unused-components': 0,
+		'quotes': 0,
+		'indent': 0,
+		'no-trailing-spaces': 0,
+		'no-tabs': 0,
+		'semi': 0
+	}
 }
